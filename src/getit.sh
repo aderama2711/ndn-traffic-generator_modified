d353b0f23fd46ac5049b16efe014a8a2da390722
aderama2711 <ade.ramadha@gmail.com>
2021-08-21 12:10:26 +0700

    delete unnecessary file

--- src/getit.sh
+++
@@ -1,5 +0,0 @@
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1kF1Up--RPKXzmpO-WBAVKCFB4M3J6avE' -O discrete_distribution_ii.h
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1DafL36rPCyt-8ESjcSWpwSKAloxEAUon' -O discrete_distribution.h
-rm ndn-traffic-client.cpp
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1oMJ6sEeNUjKt1iMcyfEgbb9FFrqlUE1x' -O ndn-traffic-client.cpp
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=11UAk58b8WTLjKF9IA3Fbfx15Cj1MYClz' -O zipf-mandelbrot.h
