0379864f14a79aade9a9cf8e039dae28a72d8ae3
aderama2711 <ade.ramadha@gmail.com>
2022-02-09 16:14:27 +0700

    fix argument for content size

--- generator.py
+++ generator.py
@@ -23,7 +23,7 @@ def main(argv):
     #checking argument
     for opt, arg in opts :
         if opt in ("-h", "--help") :
-            print "Generator for \"ndn-traffic-generator\" configuration file!\nOptions :\n\t-h [ --help ] arg \t\tPrint this help message and hope it helps :)\n\t-s [ --size ] arg \t\tSet number of prefix, default = 100\n\t-n [ --name ] arg \t\tSet name for every TrafficPattern, format = (name)/traffic(iteration_number)\n\t\t\t\t\tExample : \n\t\t\t\t\tif input name = /ndn/telu/fte/%C1.Router/router2 \n\t\t\t\t\tthen name for TrafficPattern 1 is /ndn/telu/fte/%%C1.Router/router2/prefix1 and so on\n\t-p [ --prefix ] arg \t\tSet prefix name, format = (prefix)(iteration_number)\n\t\t\t\t\tExample : \n\t\t\t\t\tif input prefix = /ndn/telu/fif/lab then prefix for Pattern Traffic 1 is /ndn/telu/fif/lab1 and so on\n\t-m [ --mode ] arg \t\tInput 1 for MustBeFresh true and 0 for MustBeFresh false, default=1\n\t-f [ --freshness ] arg \t\tSet FreshnessPeriod in ms, default = 1"
+            print "Generator for \"ndn-traffic-generator\" configuration file!\nOptions :\n\t-h [ --help ] arg \t\tPrint this help message and hope it helps :)\n\t-s [ --size ] arg \t\tSet number of prefix, default = 100\n\t-n [ --name ] arg \t\tSet name for every TrafficPattern, format = (name)/traffic(iteration_number)\n\t\t\t\t\tExample : \n\t\t\t\t\tif input name = /ndn/telu/fte/%C1.Router/router2 \n\t\t\t\t\tthen name for TrafficPattern 1 is /ndn/telu/fte/%%C1.Router/router2/prefix1 and so on\n\t-p [ --prefix ] arg \t\tSet prefix name, format = (prefix)(iteration_number)\n\t\t\t\t\tExample : \n\t\t\t\t\tif input prefix = /ndn/telu/fif/lab then prefix for Pattern Traffic 1 is /ndn/telu/fif/lab1 and so on\n\t-m [ --mode ] arg \t\tInput 1 for MustBeFresh true and 0 for MustBeFresh false, default=1\n\t-f [ --freshness ] arg \t\tSet FreshnessPeriod in ms, default = 1\n\t-c [ --contentsize ] arg \t\tSet ContentSize in Bytes, default = 1000"
             sys.exit()
         elif opt in ("-s", "--size") :
             size = int(arg)
@@ -48,7 +48,7 @@ def main(argv):
     file = open("client.conf","wb")
     string=""
     for i in range(1,size+1) :
-        string = string + "\n\nTrafficPercentage=1\nName="+name+"/traffic"+str(i)+"\nExpectedContent="+prefix+str(i)+"\nMustBeFresh="+str(mode)+"\nContentBytes="+str(csize)
+        string = string + "\n\nTrafficPercentage=1\nName="+name+"/traffic"+str(i)+"\nExpectedContent="+prefix+str(i)+"\nMustBeFresh="+str(mode)
     file.write(string.encode())
     file.close
 
@@ -58,7 +58,7 @@ def main(argv):
     file = open("server.conf","wb")
     string = ""
     for i in range(1,size+1) :
-        string = string + "\n\nName="+name+"/traffic"+str(i)+"\nContent="+prefix+str(i)+"\nFreshnessPeriod="+str(freshness)
+        string = string + "\n\nName="+name+"/traffic"+str(i)+"\nContent="+prefix+str(i)+"\nFreshnessPeriod="+str(freshness)+"\nContentBytes="+str(csize)
     file.write(string.encode())
     file.close
 
