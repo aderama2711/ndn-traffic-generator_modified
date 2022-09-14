cad56f391a247a23220c13854722f2bef3322618
aderama2711 <ade.ramadha@gmail.com>
2022-09-14 10:57:12 +0700

    update generator.py

--- generator.py
+++ generator.py
@@ -8,14 +8,15 @@ def main(argv):
     #initialize default
     size = 100
     name = "/ndn/telu/fif/%C1.Router/router2/"
-    prefix = "/ndn/telu/fif/lab/ndn"
+    #prefix = "/ndn/telu/fif/lab/ndn"
     mode = True
-    freshness = 1
-    csize = 1000
+    freshness = 60000
+    csize = 1024
+    char = ''.join('A' for _ in range(int(csize)))
 
     #initialize argument
     try:
-        opts, args = getopt.getopt(argv,"hs:n:p:m:f:c:",["help","size=","name=","prefix=","mode=","freshness=","contentsize="])
+        opts, args = getopt.getopt(argv,"hs:n:m:f:c:",["help","size=","name=","mode=","freshness=","contentsize="])
     except getopt.GetoptError:
         print ('use -h or --help for help')
         sys.exit(2)
@@ -23,14 +24,12 @@ def main(argv):
     #checking argument
     for opt, arg in opts :
         if opt in ("-h", "--help") :
-            print ("Generator for \"ndn-traffic-generator\" configuration file!\nOptions :\n\t-h [ --help ] arg \t\tPrint this help message and hope it helps :)\n\t-s [ --size ] arg \t\tSet number of prefix, default = 100\n\t-n [ --name ] arg \t\tSet name for every TrafficPattern, format = (name)/traffic(iteration_number)\n\t\t\t\t\tExample : \n\t\t\t\t\tif input name = /ndn/telu/fte/%C1.Router/router2 \n\t\t\t\t\tthen name for TrafficPattern 1 is /ndn/telu/fte/%%C1.Router/router2/prefix1 and so on\n\t-p [ --prefix ] arg \t\tSet prefix name, format = (prefix)(iteration_number)\n\t\t\t\t\tExample : \n\t\t\t\t\tif input prefix = /ndn/telu/fif/lab then prefix for Pattern Traffic 1 is /ndn/telu/fif/lab1 and so on\n\t-m [ --mode ] arg \t\tInput 1 for MustBeFresh true and 0 for MustBeFresh false, default=1\n\t-f [ --freshness ] arg \t\tSet FreshnessPeriod in ms, default = 1\n\t-c [ --contentsize ] arg \t\tSet ContentSize in Bytes, default = 1000")
+            print ("Generator for \"ndn-traffic-generator\" configuration file!\nOptions :\n\t-h [ --help ] arg \t\tPrint this help message and hope it helps :)\n\t-s [ --size ] arg \t\tSet number of prefix, default = 100\n\t-n [ --name ] arg \t\tSet name for every TrafficPattern, format = (name)/traffic(iteration_number)\n\t\t\t\t\tExample : \n\t\t\t\t\tif input name = /ndn/telu/fte/%C1.Router/router2 \n\t\t\t\t\tthen name for TrafficPattern 1 is /ndn/telu/fte/%%C1.Router/router2/prefix1 and so on\n\t-m [ --mode ] arg \t\tInput 1 for MustBeFresh true and 0 for MustBeFresh false, default=1\n\t-f [ --freshness ] arg \t\tSet FreshnessPeriod in ms, default = 60000\n\t-c [ --contentsize ] arg \t\tSet ContentSize in Bytes, default = 1024")
             sys.exit()
         elif opt in ("-s", "--size") :
             size = int(arg)
         elif opt in ("-n", "--name") :
             name = arg
-        elif opt in ("-p", "--prefix") :
-            prefix = arg
         elif opt in ("-m", "--mode") :
             if(int(arg) == 1) :
                 mode = True
@@ -49,7 +48,7 @@ def main(argv):
     file = open("client.conf","wb")
     string=""
     for i in range(1,size+1) :
-        string = string + "\n\nTrafficPercentage=1\nName="+name+"/traffic"+str(i)+"\nExpectedContent="+prefix+str(i)+"\nMustBeFresh="+str(mode)
+        string = string + "\n\nTrafficPercentage=1\nName="+name+"/traffic"+str(i)+"\nExpectedContent="+char+"\nMustBeFresh="+str(mode)
     file.write(string.encode())
     file.close
 
@@ -59,7 +58,7 @@ def main(argv):
     file = open("server.conf","wb")
     string = ""
     for i in range(1,size+1) :
-        string = string + "\n\nName="+name+"/traffic"+str(i)+"\nContent="+prefix+str(i)+"\nFreshnessPeriod="+str(freshness)+"\nContentBytes="+str(csize)+"\nContent="+char
+        string = string + "\n\nName="+name+"/traffic"+str(i)+"\nFreshnessPeriod="+str(freshness)+"\nContentBytes="+str(csize)+"\nContent="+char
     file.write(string.encode())
     file.close
 
@@ -69,7 +68,7 @@ def main(argv):
     file = open("initial.sh","wb")
     string = ""
     for i in range(1,size+1) :
-        string = string + "\nnlsrc advertise "+prefix+str(i)
+        string = string + "\nnlsrc advertise "+name+"/traffic"+str(i)
     file.write(string.encode())
     file.close
 
